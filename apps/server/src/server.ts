6dba153e935cb30740c6123aa5906630d3cadb73
Antonio Lopes <antoniolopesg@protonmail.com>
2023-02-15 22:51:00 -0300

    feat(server): connection to mongo

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,9 +1,16 @@
 import http from 'node:http'
+import mongoose from 'mongoose'
 import { env } from './env'
 import { app } from './app'
 
-http
-  .createServer(app.callback())
-  .listen(env.PORT, () => {
-    console.log(`Listening at ${env.PORT}`)
+mongoose.set('strictQuery', true)
+
+mongoose
+  .connect(env.MONGO_URI)
+  .then(() => {
+    http
+      .createServer(app.callback())
+      .listen(env.PORT, () => {
+        console.log(`Listening at ${env.PORT}`)
+      })
   })
